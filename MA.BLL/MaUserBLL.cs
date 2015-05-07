f5d62681ef5cb4b220d1c895331d0de55b76ed27
zhz <jgl5981@hotmail.com>
2015-05-07 17:09:07 +0800

    0507

---
+++ MA.BLL/MaUserBLL.cs
@@ -0,0 +1,19 @@
+﻿using MA.DAL;
+using MA.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MA.BLL
+{
+    public class MaUserBLL
+    {
+        MaUserDAL _maUserDal = new MaUserDAL();
+        public IList<MaUser> GetList()
+        {
+            return _maUserDal.GetList();
+        }
+    }
+}
