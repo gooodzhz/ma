e87207b7b8baace462f6d8672517cdcf1d6e48b2
zhz <jgl5981@hotmail.com>
2015-05-11 08:50:14 +0800

    6

---
+++ MA.DAL/DAL/Entity.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+using System.Data;
+using System.Collections.Generic;
+using MA.Model;
+namespace MA.DAL
+{
+		
+	public partial class MaUserDAL
+    {
+		public int Save(MaUser model)
+		{
+			MySqlParameter[] paras = new MySqlParameter[] { 
+				 new MySqlParameter("name", model.Name),
+				 new MySqlParameter("pwd", model.Pwd),
+				 new MySqlParameter("nickname", model.Nickname),
+				 new MySqlParameter("date", model.Date)
+			};
+
+			string sql = "INSERT INTO ma_user(name, pwd, nickname, date) VALUES(?name, ?pwd, ?nickname, ?date)";
+		    return DBHelper.NoneQuery(sql, paras);
+		}
+		
+		public int Update(MaUser model)
+        {
+
+            MySqlParameter[] paras = new MySqlParameter[] { 
+				 new MySqlParameter("id", model.Id),
+				 new MySqlParameter("name", model.Name),
+				 new MySqlParameter("pwd", model.Pwd),
+				 new MySqlParameter("nickname", model.Nickname),
+				 new MySqlParameter("date", model.Date)
+			};
+
+			string sql = "UPDATE ma_user SET name = ?name, pwd = ?pwd, nickname = ?nickname, date = ?date WHERE id = ?id";
+            return DBHelper.NoneQuery(sql, paras);
+
+        }
+
+		public int Delete(int id)
+        {
+            string sql = string.Format("DELETE FROM ma_user WHERE id = {0}", id);
+            return DBHelper.NoneQuery(sql);
+
+        }
+
+		public MaUser GetById(int id)
+        {
+            string sql = string.Format("SELECT * FROM ma_user WHERE id = {0}", id);
+            DataTable table = DBHelper.GetDateTable(sql);
+			List<MaUser> list = new DatatableFill<MaUser>().FillModel(table);
+            if (list == null || list.Count == 0) return null;
+            return list[0];
+        }
+		
+		public bool Exist(string where)
+        {
+            int n = Count(where);
+            return n > 0 ? true : false;
+        }
+
+        public int Count(string where)
+        {
+            string sql = string.Format("SELECT COUNT(1) FROM ma_user WHERE {0}", where);
+            DataTable table = DBHelper.GetDateTable(sql);
+            return Convert.ToInt32(table.Rows[0][0]);
+        }
+
+        public int Count()
+        {
+            return Count(" 1 = 1 ");
+        }
+
+		public List<MaUser> GetList()
+        {
+            string sql = "SELECT * FROM ma_user";
+            DataTable table = DBHelper.GetDateTable(sql);
+			List<MaUser> list = new DatatableFill<MaUser>().FillModel(table);
+            return list;
+
+        }
+
+		public List<MaUser> GetList(string where)
+        {
+            string sql = string.Format("SELECT * FROM ma_user WHERE {0};", where);
+            DataTable table = DBHelper.GetDateTable(sql);
+            return new DatatableFill<MaUser>().FillModel(table);
+        }
+
+        public List<MaUser> GetList(string where, PagerInfo info)
+        {
+            info.Count = Count(where);
+            string sql = string.Format(" {0} LIMIT {1}, {2} ", where,  info.Index, info.Size);
+            return GetList(sql);
+        }
+		
+        public int Truncate()
+        {
+            return DBHelper.NoneQuery("TRUNCATE TABLE ma_user");
+        }
+    }
+
+}
+
---
+++ MA.Model/Entity/Entity.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Data;
+using System.Collections.Generic;
+
+namespace MA.Model
+{
+	public partial class MaUser
+    {
+		/// <summary>
+        /// Id
+        /// </summary>
+		public int Id { get; set; }
+		/// <summary>
+        /// 用户名
+        /// </summary>
+		public string Name { get; set; }
+		/// <summary>
+        /// 密码
+        /// </summary>
+		public string Pwd { get; set; }
+		/// <summary>
+        /// 昵称
+        /// </summary>
+		public string Nickname { get; set; }
+		/// <summary>
+        /// 注册时间
+        /// </summary>
+		public DateTime Date { get; set; }
+		
+     }
+
+	public partial class MaUserSummary
+    {
+		/// <summary>
+        /// Id
+        /// </summary>
+        public static string IdSummary { get{return "Id";} }
+		/// <summary>
+        /// Id字符长度
+        /// </summary>
+        public static int IdCharLength { get{return 11;} }
+		/// <summary>
+        /// 用户名
+        /// </summary>
+        public static string NameSummary { get{return "用户名";} }
+		/// <summary>
+        /// 用户名字符长度
+        /// </summary>
+        public static int NameCharLength { get{return 50;} }
+		/// <summary>
+        /// 密码
+        /// </summary>
+        public static string PwdSummary { get{return "密码";} }
+		/// <summary>
+        /// 密码字符长度
+        /// </summary>
+        public static int PwdCharLength { get{return 50;} }
+		/// <summary>
+        /// 昵称
+        /// </summary>
+        public static string NicknameSummary { get{return "昵称";} }
+		/// <summary>
+        /// 昵称字符长度
+        /// </summary>
+        public static int NicknameCharLength { get{return 50;} }
+		/// <summary>
+        /// 注册时间
+        /// </summary>
+        public static string DateSummary { get{return "注册时间";} }
+		/// <summary>
+        /// 注册时间字符长度
+        /// </summary>
+        public static int DateCharLength { get{return 0;} }
+		
+     }
+}
+
